8b1fa7e2763cc4527dfe5ab6b42fd2c6c2543d0e
khalkiasma2022 <118854005+khalkiasma2022@users.noreply.github.com>
2026-04-01 14:39:33 +0100

    Add Test class with main method for Java build

---
+++ src/application/Test.java
@@ -0,0 +1,7 @@
+package application;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Félicitations ! Le build Java fonctionne parfaitement sur Jenkins !");
+    }
+}
